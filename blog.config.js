afbd9471e767ed3845ca103b1a7e9f7a88dc6edf
Sabrina <admin@lastcn.com>
2023-03-14 04:19:10 +0800

    测试

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '韩菲羽', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '有了你以后梦到的世界都是彩色的。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://closertoyou.io', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '我的刺猬女孩, 剧集,照片', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '我的刺猬女孩,剧集,照片', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
